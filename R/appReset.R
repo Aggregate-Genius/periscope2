eacb4f41bccb64724dc1ce5d180da817907cefc3
Mohammed Ali <mohammed@aggregate-genius.com>
2022-12-04 02:15:47 -0800

    - Documented "appReset" module

--- R/appReset.R
+++ R/appReset.R
@@ -1,9 +1,45 @@
 # -------------------------------------
 # -- Application Reset Button Module --
-# -- Reload Application Sessoon      --
+# -- Reload Application Session      --
 # -------------------------------------
 
-# Module UI Function
+#' appResetButton
+#'
+#' Creates a toggle button to reset application session. Upon pressing on the
+#'  button, its state is flipped to cancel application reload with application
+#'  and console warning messages indicating that the application will be
+#'  reloaded.
+#'
+#'  User can either resume reloading application session or cancel reloading
+#'  process which will also generate application and console messages to
+#'  indicate reloading status and result.
+#'
+#'
+#' @param id character id for the object
+#'
+#' @section Button Features:
+#' \itemize{
+#'     \item Initial state label is "Application Reset" with warning status
+#'     \item Reloading state label is "Cancel Application Reset" with danger
+#'           status
+#' }
+#'
+#' @section Example:
+#' \code{appReset(id = 'appResetId', logger = logger)}
+#'
+#' @section Shiny Usage:
+#' Call this function at any place in UI section.
+#'
+#' It is paired with a call to \code{appResetButton(id, ...)}
+#' in server
+#'
+#'
+#' @examples
+#' # Inside ui_body.R or ui_left_sidebar.R
+#'
+#' appReset(id = 'appResetId', logger = logger)
+#'
+#'
 #' @export
 appResetButton <- function(id) {
     ns <- shiny::NS(id)
@@ -25,12 +61,36 @@ appResetButton <- function(id) {
     )
 }
 
-# Module Server Function
+
+#' appReset Module Server Function
+#'
+#' Server-side function for the appResetButton  This is a custom
+#' high-functionality button for session reload. The server function is used to
+#'  provide module configurations.
+#'
+#' @param id             - the ID of the Module's UI element
+#' @param reset_wait     - period to wait before session reload (in milliseconds)
+#' @param alert_location - where to display module related messages
+#'       (defult = "bodyAlert")
+#' @param logger         - logger to use
+#'
+#'
+#' @section Shiny Usage:
+#' This function is not called directly by consumers - it is accessed in
+#' server_local.R (or similar file) using the same id provided in \code{downloadFileButton}:
+#'
+#' \strong{\code{appReset(id = "appResetId", logger = ss_userAction.Log)}}
+#'
+#' @examples
+#' # Inside server_local.R
+#'
+#' #appReset(id = "appResetId", logger = ss_userAction.Log)
+#'
 #' @export
-appReset <- function(id, 
-                      reset_wait     = 5000, 
-                      alert_location = "bodyAlert", 
-                      logger) {
+appReset <- function(id,
+                     reset_wait     = 5000,
+                     alert_location = "bodyAlert",
+                     logger) {
     shiny::moduleServer(
         id,
         function(input, output, session) {
@@ -39,10 +99,10 @@ appReset <- function(id,
                     logwarn("Alert location must be one of standard alert values: 'bodyAlert', 'headerAlert', 'footerAlert', 'leftAlert', 'rightAlert'", logger = logger)
                     logwarn("Setting alert location to 'bodyAlert'")
                 }
-                
+
                 pending  <- shiny::isolate(input$resetPending)
                 waittime <- reset_wait
-                
+
                 if (!is.null(pending)) {
                     closeResetAlert(alert_location)
                     if (input$resetButton && !(pending)) {
@@ -73,7 +133,7 @@ appReset <- function(id,
                                             title    = "Reset Application",
                                             closable = TRUE,
                                             content  = "The application reset was canceled."))
-                        
+
                         shinyWidgets::updatePrettyToggle(session,
                                                          "resetPending",
                                                          value = FALSE)
@@ -84,5 +144,5 @@ appReset <- function(id,
                     }
                 }
             })
-        })   
+        })
 }
