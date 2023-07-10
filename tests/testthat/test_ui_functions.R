4788f44d213d4bd4bb28e231b10e87fe8b7508b5
Mohammed Ali <mohammed@aggregate-genius.com>
2023-07-10 01:58:27 -0700

    - Added unit test for invalid style for announcements

--- tests/testthat/test_ui_functions.R
+++ tests/testthat/test_ui_functions.R
@@ -318,6 +318,9 @@ test_that("load_announcements - parsing error", {
 test_that("load_announcements function parameters", {
     expect_null(create_announcements(start_date = "2222-11-26",
                                      end_data   = "2222-12-26"))
+    expect_null(create_announcements(start_date = "2022-11-26",
+                                     end_data   = "2222-12-26",
+                                     style      = "not-style"))
     expect_null(create_announcements(start_date        = "11-26-2222",
                                      end_data          = "12-26-2222",
                                      start_date_format = "%m-%d-%Y",
