f983bf402a271471fffd78e9c8cc36f1603ef37e
Mohammed Ali <mohammed@aggregate-genius.com>
2023-10-26 05:22:30 -0700

    - Covered all addins server functions successfully

--- tests/testthat/test_addins.R
+++ tests/testthat/test_addins.R
@@ -1,6 +1,53 @@
 context("periscope2 - package addins")
 local_edition(3)
 
-test_that("create alert - announcement addin", {
+test_that("announcement addin - UI", {
     expect_snapshot_output(announcement_addin_UI())
+})
+
+
+test_that("announcement addin - server", {
+    expect_true(stopApp(announcementConfigurationsAddin()))
+
+    testServer(announcement_addin_server,
+               expr = {
+                   session$setInputs(startPicker       = "2023-10-27",
+                                     endPicker         = "2023-10-28",
+                                     announcement_text = "test body",
+                                     auto_close        = 30,
+                                     style             = "info",
+                                     title             = "test title")
+                   session$setInputs(downloadConfig = 0)
+                   announce <- yaml::read_yaml(output$downloadConfig)
+                   expect_equal(announce[["start_date"]], "2023-10-27")
+                   expect_equal(announce[["end_date"]], "2023-10-28")
+                   expect_null(announce[["start_date_format"]])
+                   expect_null(announce[["end_date_format"]])
+                   expect_equal(announce[["auto_close"]], 30)
+                   expect_equal(announce[["style"]], "info")
+                   expect_equal(announce[["title"]], "test title")
+                   expect_equal(announce[["text"]], "test body")
+
+                   session$setInputs(announcement_text = "test body2",
+                                     auto_close        = 10)
+                   session$setInputs(downloadConfig = 1)
+
+                   announce <- yaml::read_yaml(output$downloadConfig)
+                   expect_equal(announce[["start_date"]], "2023-10-27")
+                   expect_equal(announce[["end_date"]], "2023-10-28")
+                   expect_null(announce[["start_date_format"]])
+                   expect_null(announce[["end_date_format"]])
+                   expect_equal(announce[["auto_close"]], 10)
+                   expect_equal(announce[["style"]], "info")
+                   expect_equal(announce[["title"]], "test title")
+                   expect_equal(announce[["text"]], "test body2")
+
+                   session$setInputs(announcement_text = NA,
+                                     auto_close        = -10)
+                   session$setInputs(downloadConfig = 2)
+                   announce <- yaml::read_yaml(output$downloadConfig)
+                   expect_equal(announce[["text"]], "NA")
+                   expect_equal(announce[["auto_close"]], -10)
+                   session$setInputs(done = 1)
+               })
 })
