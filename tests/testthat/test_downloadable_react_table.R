e070715147056e730f8f97b52e31eaac090df321
Mohammed Ali <mohammed@aggregate-genius.com>
2025-07-15 02:27:52 -0700

    - Added valid data unit tests

--- tests/testthat/test_downloadable_react_table.R
+++ tests/testthat/test_downloadable_react_table.R
@@ -20,3 +20,29 @@ test_that("downloadableReactTableUI - no download types", {
     expect_true(grepl('style="display:none">', table_ui[[1]], fixed = TRUE))
     expect_true(grepl('id="myid2-reactTableOutputID"', table_ui[[2]], fixed = TRUE))
 })
+
+# helper functions
+get_mtcars_data <- reactive({
+    head(mtcars)
+})
+
+
+test_that("downloadableReactTable - valid data", {
+    testServer(downloadableReactTable,
+               args = list(table_data = get_mtcars_data),
+               expr = {
+                   expect_true(grepl(paste0(names(get_mtcars_data()), collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(rownames(get_mtcars_data()), collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$mpg, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$cyl, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$disp, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$hp, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$drat, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$wt, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$qsec, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$vs, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$am, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$gear, collapse = "|"), output$reactTableOutputID))
+                   expect_true(grepl(paste0(get_mtcars_data()$carb, collapse = "|"), output$reactTableOutputID))
+               })
+})
