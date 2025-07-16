682109e2bc437cf8206dc63f371cc83dd4101471
Mohammed Ali <mohammed@aggregate-genius.com>
2025-07-16 06:44:32 -0700

    - Updated module documentation

--- R/downloadableReactTable.R
+++ R/downloadableReactTable.R
@@ -7,7 +7,7 @@
 #'
 #' downloadableReactTable module is extending \code{?reactable} package table functions by creating
 #' a custom high-functionality table paired with \link[periscope2]{downloadFile} button.
-#' The table has search and highlight functionality, infinite scrolling, sorting by columns and
+#' The table has the following default functionality:search, highlight functionality, infinite scrolling, sorting by columns and
 #' returns a reactive dataset of selected items.
 #'
 #' \link[periscope2]{downloadFile} button will be hidden if \code{downloadableReactTableUI} parameter
@@ -49,8 +49,6 @@
 #' in server.R
 #'
 #' @seealso \link[periscope2]{downloadableReactTable}
-#' @seealso \link[periscope2]{downloadableTableUI}
-#' @seealso \link[periscope2]{downloadableTable}
 #' @seealso \link[periscope2]{downloadFile}
 #' @seealso \link[periscope2]{logViewerOutput}
 #' @seealso \link[periscope2]{downloadFile}
@@ -67,7 +65,7 @@
 #'                             downloadtypes = c("csv", "tsv"),
 #'                             hovertext     = "Download the data here!",
 #'                             contentHeight = "300px",
-#'                             singleSelect  = FALSE)))),
+#'                             singleSelect  = TRUE)))),
 #'    server = function(input, output) {
 #'      downloadableReactTable("object_id1")})
 #'}
@@ -132,8 +130,6 @@ downloadableReactTableUI <- function(id,
 #' \strong{\code{downloadableReactTable(id)}}
 #'
 #' @seealso \link[periscope2]{downloadableReactTableUI}
-#' @seealso \link[periscope2]{downloadableTableUI}
-#' @seealso \link[periscope2]{downloadableTable}
 #' @seealso \link[periscope2]{downloadFileButton}
 #' @seealso \link[periscope2]{logViewerOutput}
 #' @seealso \link[periscope2]{downloadFile}
@@ -150,7 +146,7 @@ downloadableReactTableUI <- function(id,
 #'                             downloadtypes = c("csv", "tsv"),
 #'                             hovertext     = "Download the data here!",
 #'                             contentHeight = "300px",
-#'                             singleSelect  = FALSE)))),
+#'                             singleSelect  = TRUE)))),
 #'    server = function(input, output) {
 #'      downloadableReactTable("object_id1")})
 #'}
